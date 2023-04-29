a003a0bb617a01e791cb2ee0fef7f49c40e72f22
DENIS LOGINOV <denis.lkg@gmail.com>
2023-04-29 21:06:25 +0600

    ExchangeResult in progress

--- src/widgets/CurrenciesRateWidget/CurrenciesRateWidget.tsx
+++ src/widgets/CurrenciesRateWidget/CurrenciesRateWidget.tsx
@@ -1,10 +1,28 @@
+import { useState } from 'react'
 import { CurrencyInputFeature, CurrencySelectFeature } from '~/features'
+import { ExchangeResultFeature } from '~/features/ExchangeResultFeature'
 
 export const CurrenciesRateWidget = () => {
+	const [exchange, setExchange] = useState({
+		amount: 1,
+		from: "USD",
+		to: "AUD"
+	})
+
+	function handleChange(name: string, value: number | string) {
+		setExchange((prev) => ({ ...prev, [name]: value }))
+	}
+
+
 	return (
 		<>
 			<CurrencyInputFeature />
 			<CurrencySelectFeature />
+			<CurrencySelectFeature />
+			<ExchangeResultFeature
+				baseCurrencyKey={exchange.from}
+				targetCurrencyKey={exchange.to}
+			/>
 		</>
 	)
 }
