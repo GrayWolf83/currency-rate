cbaa6f11f96671fd603f972b986362273587436e
AntonKulagin <i-gorusich@mail.ru>
2023-04-30 11:41:52 +0300

    button rotate

--- src/entities/components/UI/MyButton.tsx
+++ src/entities/components/UI/MyButton.tsx
@@ -1,18 +1,28 @@
 import { Button } from 'antd'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { SwapOutlined } from '@ant-design/icons'
 
 export interface IMyButton {
-	onClick: () => void
+  onClick: () => void
 }
 
 export const MyButton: FC<IMyButton> = ({ onClick }) => {
-	return (
-		<Button
-			shape='circle'
-			icon={<SwapOutlined />}
-			size='large'
-			onClick={onClick}
-		/>
-	)
+  const [newStyle, setNewStyle] = useState(false)
+
+  const handleClick = () => {
+    onClick()
+    setNewStyle((prev) => !prev)
+  }
+
+  return (
+    <div>
+      <Button
+        style={{ transform: newStyle ? 'rotate(180deg)' : 'rotate(0deg)' }}
+        shape='circle'
+        icon={<SwapOutlined />}
+        size='middle'
+        onClick={handleClick}
+      />
+    </div>
+  )
 }
